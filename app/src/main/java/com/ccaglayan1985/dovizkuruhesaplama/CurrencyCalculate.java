3e512b7d2f1239cce120bea7ee8019d9e0d8d921
cihan caglayan <ccaglayan1985@gmail.com>
2017-03-26 10:20:07 +0300

    düzeltmeler yapıldı.

---
+++ app/src/main/java/com/ccaglayan1985/dovizkuruhesaplama/CurrencyCalculate.java
@@ -0,0 +1,36 @@
+package com.ccaglayan1985.dovizkuruhesaplama;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class CurrencyCalculate extends AppCompatActivity {
+
+    private Spinner spnBuySell = null;
+    private TextView txtAmountHeader = null, txtType = null;
+    private EditText edtAmount = null, edtCent = null;
+
+    private TextView txtAmountHeader2 = null, txtType2 = null;
+    private TextView txtAmountHeader2 = null, txtType2 = null;
+
+
+    private EditText edtAmount2 = null, edtCent2 = null;
+
+    private Button
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency_calculate);
+
+    }
+
+    private void initView() {
+
+
+    }
+}
+
